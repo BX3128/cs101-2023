ec99d9c05cc8095a4d50b693ebabd01b632b8436
BX3128 <binx1394@gmail.com>
2023-03-23 14:34:52 +0800

    Lab4

--- lab4/main.c
+++ lab4/main.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 void replace_all(char* str, char oldChar, char newChar) {
-    int i = 0;
-    while (str[i] != '\0') {
-        if (str[i] == oldChar) {
-            str[i] = newChar;
-        }
-        i++;
+    while (*str != '\0') {
+       *str == oldChar ? *str = newChar : 0;
+       str++;
     }
 }
 
@@ -19,4 +16,36 @@ int main() {
     printf("\n\nString after replacing %c with %c : \n%s\n", oldChar, newChar, str);
 
     return 0;
+}
+
+// memo
+/*
+
+// cost: 9 lines
+void replace_all(char* str, char oldChar, char newChar) {
+    int i = 0;
+    while (str[i] != '\0') {
+        if (str[i] == oldChar) {
+            str[i] = newChar;
+        }
+        i++;
+    }
+}
+
+// cost: 8 lines
+void replace_all(char* str, char oldChar, char newChar) {
+    while (*str != '\0') {
+        if (*str == oldChar) {
+            *str = newChar;
+        }
+        str++;
+    }
+}
+
+// cost: 6 lines
+void replace_all(char* str, char oldChar, char newChar) {
+    while (*str != '\0') {
+        *str == oldChar ? *str = newChar : 0;
+        str++;
+    }
 }
