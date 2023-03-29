5f2cbd467aa39dbb697757931851af1a75aaf909
BX3128 <binx1394@gmail.com>
2023-03-29 23:12:57 +0800

    HW3

--- hw3/main.c
+++ hw3/main.c
@@ -1,17 +1,26 @@
-#include <stdio.h>  // printf, fprintf, fseek, ftell
-#include <stdlib.h> // 
-#include <time.h>   // time, ctime
-#include <string.h> // strcpy
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-// macro
-#define RECORD "record.bin"
+// program parameters
+#define MAXLOT 69    // max lottery number
+#define SPLOT 10     // max special lottery number
+                     // lottery number must begin with 1
+#define NORMAL 6     // how many normal lot number
+                     // only 1 special lot number allowed
+#define MAX_GEN 5    // max lot number tuples to be generated
+#define LOTTO_M 100
 
-#define MAXLOT 69   // max possible lot number in a set
-#define SPLOT 10    // max possible special lot number in a set
-#define NORMAL 6    // # of normal lot numbers in a set
-#define MAX_GEN 5   // # of sets of lot numbers
+// global variables
+int lot[MAXLOT];     // lottery tickets
 
-#define ASK_LOT_SETS 1
+// function prototypes
+void init_lot(void); // initialize lot[]
+void shuf_lot(void); // shuffle the lot[]
+void sort_lot(void); // sort the first NORMAL lot numbers
+void swap_lot(int i, int j); // swap the i and j elements of lot[]
+int sp_lot(void);    // find the special number
 
 // struct
 typedef struct {
@@ -21,77 +30,124 @@ typedef struct {
     int lotto_set[5][7];
 } lotto_record_t;
 
-// function prototypes
-int query(int instruction);             // query info from user
-int procf(FILE* fp, int instruction,    // read/write file data
-          int lotto_record_size,
-          lotto_record_t lottos[]);
+// main()
+int main(int argc, char* argv[]) {
+    srand((unsigned) time(NULL)); // random seed
 
-
-int main() {
-    srand((unsigned) time(NULL));  // random seed
-
-    time_t ct;                  // current time
-    char ct_str[80];            // current time: formatted string
-    int n = 1;                  // # of sets (1-5)
-    lotto_record_t lottos[100]; // # of tickets (max 5 sets per ticket)
-    FILE* fp_txt;               // file pointer to lotto[*].txt
-    FILE* fp_bin;               // file pointer to record.bin
-
-    fprintf(stdout, "歡迎光臨長庚樂透彩購買機台\n");
-    if (n = query(ASK_LOT_SETS)) {
-        if () {
-            
-            fclose(fp_bin); 
-        } else {
-            if (!(fp_bin = fopen(RECORD, "wb"))) {
-                fprintf(stderr, "無法建立檔案 %s\n", RECORD);
-                exit(1);
-            }
-            fseek(fp, 0, SEEK_END);
-            int size = ftell(fp);
-            if (size % sizeof(lotto_record_t) != 0) {
-                fprintf(stderr, RECORD" 檔案損毀，重建已購買資料庫。\n");
+    time_t curtime;
+    char time_str[80];  // string buffer to store current time
+    int n;              // how many tuples of lot to buy?
+    FILE* fp;
+    FILE* ftxt;
+    int last_rec = -1;
+    int ticket_no = 0;
+    char TXT[80];
+    lotto_record_t lottos[LOTTO_M];
+    
+    printf("歡迎光臨長庚樂透彩購買機台\n");
+    fp = fopen("record.bin", "rb");
+    if (fp) {
+        for (int i=0; i<LOTTO_M; i++) {
+            int c = fread(&lottos[i], sizeof(lotto_record_t), 1, fp);
+            if (!c) {
+                last_rec = i - 1;
+                break;
             }
         }
+        fclose(fp);
+        fp = fopen("record.bin", "ab");
+    } else {
+        fp = fopen("record.bin", "wb");
     }
+    ticket_no++;
+    sprintf(TXT, "lotto[%05d].txt", ticket_no);
+fclose(fp);
+    printf("請問您要買幾組樂透彩：");
+    scanf("%d", &n);
+    printf("已為您購買的 %d 組樂透組合輸出至 lotto[%05d].txt\n", n, ticket_no);
+
+    ftxt = fopen(TXT, "w");
+    fprintf(fp, "========= lotto649 =========\n");
+
     time(&curtime);
-    ctime(&curtime);
-}
+    // ctime() attaches a newline (0x0A) charaacter, i.e., "%s\n"
+    // We got to remove it manually.
+    strcpy(time_str, ctime(&curtime));
+    for (int i=0; i<sizeof(time_str)/sizeof(time_str[0]); i++) {
+        if (time_str[i] == '\0') {
+            time_str[i-1] = '\0';
+            break;
+        }
+    }
+    fprintf(ftxt, "========+ No.%05d +========\n", ticket_no);
+    fprintf(fp, "= %s =\r\n", time_str);
 
-int query(int instruction) {
-    switch (instruction) {
-    case 1:
-        int n;
-        fprintf(stdout, "請問您要買幾組樂透彩：");
-        while (1) {
-            signed char flag = scanf("%d", &n);
-            if (flag < 0 || n < 0 || n > 5) {
-                fprintf(stderr, "請輸入數字 1 到 5 以決定要購買的彩券組數\n");
-                continue;
-            }
-            if (flag == 0) {
-                fprintf(stderr, "您沒有輸入數字，預設為您購買 1 組樂透彩。\n");
-                n = 1;
-                break;
-            }
-            if (n >= 0 && n <= 5) {
-                break;
+    init_lot();  // initialize the lot[] array
+
+    for (int i=0; i<MAX_GEN; i++) {
+        shuf_lot(); // shuffle lot[] array
+        sort_lot(); // sort the first MAXLOT elements in lot[]
+        fprintf(fp, "[%1d]: ",i+1);
+        for (int j=0; j<NORMAL; j++) {
+            if (i<n) {
+                fprintf(fp, "%02d ", lot[j]);
+            } else {
+                fprintf(fp, "-- ");
             }
         }
-        return n;
-        break;
-    default:
-        return 0;
-        break;
+        if (i<n) {
+            fprintf(fp, "%02d\n", sp_lot());
+        } else {
+            fprintf(fp, "--\n");
+        }
+    }
+
+    fprintf(ftxt, "========= cise@CGU =========\n");
+    fclose(ftxt);
+    return 0;
+}
+
+void init_lot(void) {
+    for (int i=0; i<MAXLOT; i++) {
+        lot[i]=i+1;
     }
 }
 
-int promptf(FILE* fp, int instruction,
-            int lotto_record_size,
-            lotto_record_t lottos[]) {
-    switch (instruction) {
-    case 1:
-        if (fp = fopen())
+void shuf_lot(void) {
+    int tmp, k;
+    for (int i=MAXLOT-1; i>0; i--) {
+        k = rand() % (i+1);
+        swap_lot(i, k);
     }
 }
+
+int sp_lot(void) {
+    for (int i=NORMAL; i<MAXLOT; i++) {
+        if (lot[i] <= SPLOT) {
+            return lot[i];
+        }
+    }
+    return -1;
+}
+
+void sort_lot(void) {
+    int tmp;
+    for (int i=0; i<NORMAL; i++) {
+        for (int j=0; j<NORMAL; j++) {
+            if (lot[i] < lot[j]) {
+                swap_lot(i, j);
+            }
+        }
+    }
+}
+
+void swap_lot(int i, int j) {
+   int tmp = lot[i];
+   lot[i] = lot[j];
+   lot[j] = tmp;
+}
+
+// Random But No Repeat
+// Adapted algorithm: pencil-and-paper method (modern approach)
+// reference:
+//   https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#Modern_method
