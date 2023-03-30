7fc3aaf60868e9550a3fc32ecd71eee7989bdb32
BX3128 <binx1394@gmail.com>
2023-03-30 14:17:56 +0800

    Lab6: function pointer

---
+++ lab5/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int calc(int (*fun_ptr)(int, int), int x, int y) {
+    return fun_ptr(x, y);
+}
+
+int add(int x, int y) {
+    return x + y;
+}
+
+int sub(int x, int y) {
+    return x - y;
+}
+
+int mul(int x, int y) {
+    return x * y;
+}
+
+int div(int x, int y) {
+    return x / y;
+}
+
+int main() {
+    printf("sum = %d\n", calc(add, 4, 2));
+    printf("dif = %d\n", calc(sub, 4, 2));
+    printf("product = %d\n", calc(mul, 4, 2));
+    printf("quotient = %d\n", calc(div, 4, 2));
+    return 0;
+}
